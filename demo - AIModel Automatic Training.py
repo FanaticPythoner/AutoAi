c22fa6f1462f7fd0e7352b13281488feb842e3d8
Nathan Trudeau <nathantrudeau@hotmail.com>
2020-03-20 00:07:58 -0400

    Add files via upload

---
+++ demo - AIModel Automatic Training.py
@@ -0,0 +1,20 @@
+# Get the dataset
+import pandas as pd
+df = pd.read_csv("Test_Dataset\\iris2.csv")
+x = df.iloc[:, 0:4]
+y = df.iloc[:, 4:]
+
+# Import AIModel
+from autoAi.AIModel import AIModel
+
+# Create the AIModel with the auto trainer enabled
+model = AIModel("MyModel_AutoTraining", baseDumpPath="Output_Models", autoTrainer=True)
+
+# Update the AIModel dataset
+model.updateDataSet(x, y, test_size=0.2)
+
+# Train the AIModel
+model.train(max_iter=50, batchSize=10, dumpEachIter=25, verboseLevel=2)
+
+# Load the best model from all trained models located in "Output_Models/MyModel_AutoTraining"
+model.loadBestModel()
