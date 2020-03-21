e6d3452342b9a0f67ebf316edcbf026bd3ca9f6a
Nathan Trudeau <nathantrudeau@hotmail.com>
2020-03-21 16:40:28 -0400

    Update demo  - AIModel Dynamic Keras Model.py

--- demo  - AIModel Dynamic Keras Model.py
+++ demo  - AIModel Dynamic Keras Model.py
@@ -11,7 +11,7 @@ from autoAi.AutoTrainer import DynamicKerasWrapper
 from autoAi.AIModel import AIModel
 
 # Create the AIModel
-model = AIModel("MyModel_CustomModelKeras", baseDumpPath="Output_Models")
+model = AIModel("MyModel_DynamicModelKeras", baseDumpPath="Output_Models")
 
 # Update the AIModel model with a DynamicKerasWrapper
 model.updateModel(DynamicKerasWrapper())
@@ -22,5 +22,5 @@ model.updateDataSet(x, y, test_size=0.2)
 # Train the AIModel
 model.train(max_iter=1000, batchSize=10, dumpEachIter=500, verboseLevel=2)
 
-# Load the best model from all trained models located in "Output_Models/MyModel_CustomModel"
+# Load the best model from all trained models located in "Output_Models/MyModel_DynamicModelKeras"
 model.loadBestModel()
