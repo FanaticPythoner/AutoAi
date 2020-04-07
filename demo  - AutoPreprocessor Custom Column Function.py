37b6a33914ff18b5856b0fa5b4a7a4ec6dd9425b
Nathan Trudeau <nathantrudeau@hotmail.com>
2020-04-06 20:54:56 -0400

    Update demo  - AutoPreprocessor Custom Column Function.py

--- demo  - AutoPreprocessor Custom Column Function.py
+++ demo  - AutoPreprocessor Custom Column Function.py
@@ -5,8 +5,8 @@ obj = AutoPreprocessor(datasetPath='Test_Dataset\\iris.csv',
                        datasetType='csv', yDataNames=['species'])
 
 # Creating a function to apply on each specified element of a given column
-def addString(column):
-    return column + ' is Amazing!'
+def addString(element):
+    return element + ' is Amazing!'
 
 # Applying the function to each element in the column 'species' before any preprocessing steps (0)
 obj.addApplyFunctionForColumn('species', addString, step=0)
